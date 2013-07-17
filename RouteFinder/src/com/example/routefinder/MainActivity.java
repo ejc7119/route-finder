463033c42183b5dd5ccfd2e8ea73f0e79d5290b7
Eric Caron <ejc7119@rit.edu>
2013-07-17 17:50:45 -0400

    Added a new route button

--- RouteFinder/src/com/example/routefinder/MainActivity.java
+++ RouteFinder/src/com/example/routefinder/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.routefinder;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	
+	Button newRouteButton;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        addListenerOnNewRouteButton();
     }
 
 
@@ -19,5 +25,20 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    public void addListenerOnNewRouteButton() {
+    	 
+		newRouteButton = (Button) findViewById(R.id.new_route);
+ 
+		newRouteButton.setOnClickListener(new View.OnClickListener() {
+ 
+			@Override
+			public void onClick(View arg0) {
+				return;
+			}
+ 
+		});
+ 
+	}
     
 }
